e9da3d1e26fb895b713d765c7db6c03299edbbcf
moataz-akram <moatazakram0@gmail.com>
2024-09-14 19:52:36 +0300

    add GoogleSignIn package

--- Package.swift
+++ Package.swift
@@ -5,17 +5,30 @@ import PackageDescription
 
 let package = Package(
     name: "BugItPackage",
+    platforms: [
+        .iOS(.v15) // Set the minimum iOS version to 15.0
+    ],
+
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BugItPackage",
             targets: ["BugItPackage"]),
     ],
+    dependencies: [
+        .package(name: "GoogleSignIn",url: "https://github.com/google/GoogleSignIn-iOS",  .upToNextMajor(from: "8.0.0")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "BugItPackage"),
+          name: "BugItPackage",
+          dependencies:
+            [
+                .product(name: "GoogleSignIn", package: "GoogleSignIn"),
+                .product(name: "GoogleSignInSwift", package: "GoogleSignIn"),
+            ]
+        ),
         .testTarget(
             name: "BugItPackageTests",
             dependencies: ["BugItPackage"]),
